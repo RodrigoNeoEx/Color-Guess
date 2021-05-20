9a98b5fdf42f80fbb9dc57bc0e99bf7586bd771c
RodrigoNeoEx <draja.acc@gmail.com>
2021-05-20 11:57:12 -0300

    create scoreContainer and moved const mainSection to global scope

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
+const mainSection = document.createElement('section');
+
 function createMainSection() {
   const bodyChild = document.querySelector('body');
-  const mainSection = document.createElement('section');
   mainSection.id = 'mainSection';
   mainSection.className = 'mainSection'
   bodyChild.appendChild(mainSection);
 }
 
 function createMainTitle() {
-  const mainSection = document.createElement('section');
   const mainTitle = document.createElement('h1');
   mainTitle.innerHTML = '🎨 Can you guess the color? 🎨';
   mainTitle.id = 'title';
   mainTitle.className = 'title';
   mainSection.appendChild(mainTitle);
+}
+
+function createSubTitle() {
+  const rgbText = document.createElement('h3');
+  rgbText.id = 'rgb-color';
+  rgbText.className = 'rgb-color'
+  mainSection.appendChild(rgbText);
+}
+
+function createScoreContainer() {
+  const placar = document.createElement('p');
+  placar.innerHTML = 'Placar: 0';
+  placar.id = 'score';
+  placar.className = 'score'
+  mainSection.appendChild(placar);
 }
